60631dd64f297aebee9002313d3bd1caaad65fe7
Alexxanderson <bien_aaron_miranda@dlsu.edu.ph>
2023-02-19 23:06:33 +0800

    WITH MAZE TEXT

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def read_maze():
+    with open('maze.txt', 'r') as f:
+        maze_size = f.readline()
+        contents = f.read()
+
+    return contents
 
 
-def print_hi(name):
+# actual main
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+    # TODO:
+    maze = read_maze()
+
+    print(maze)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
