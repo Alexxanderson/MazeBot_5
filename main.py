f66922cdb633a5a54d4a74f3ee0c38547d095f28
Andre0819 <andre_aquino@dlsu.edu.ph>
2023-02-26 22:43:11 +0800

    normal manhattan w fixed heuristic decision-making

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ def draw_maze(screen, maze, block_size):
     # define colors
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
-    GRAY = (128, 128, 128)
+    GRAY = (75, 75, 75)
     GREEN = (0, 255, 0)
     RED = (255, 0, 0)
     BLUE = (0, 0, 255)
     YELLOW = (255,255,0)
     ORANGE = (255,165,0)
+    LIGHTGRAY = (150,150,150)
+    LIGHTORANGE = (200,200,100)
 
     # draw maze as a grid
     for i in range(len(maze)):
@@ -29,13 +31,13 @@ def draw_maze(screen, maze, block_size):
                 pygame.draw.rect(screen, GREEN, rect)
                 pygame.draw.rect(screen, BLACK, rect, 2)
             elif maze[i][j] == 'x':  # traversed
-                pygame.draw.rect(screen, GRAY, rect)
+                pygame.draw.rect(screen, LIGHTORANGE, rect)
                 pygame.draw.rect(screen, BLACK, rect, 2)
             elif maze[i][j] == '*':  # optimal path
                 pygame.draw.rect(screen, ORANGE, rect)
                 pygame.draw.rect(screen, BLACK, rect, 2)
             else:  # unexplored
-                pygame.draw.rect(screen, BLACK, rect)
+                pygame.draw.rect(screen, GRAY, rect)
                 pygame.draw.rect(screen, BLACK, rect, 2)
 
 def print_explored_gui(maze, result):
@@ -43,7 +45,7 @@ def print_explored_gui(maze, result):
     pygame.init()
 
     # set up screen
-    block_size = 40
+    block_size = 10
     width = len(maze[0]) * block_size
     height = len(maze) * block_size
     screen = pygame.display.set_mode((width+300, height))
@@ -67,7 +69,7 @@ def print_explored_gui(maze, result):
 
         draw_maze(screen, maze, block_size)
         pygame.display.update()
-        time.sleep(0.1)
+        time.sleep(0.03)
     if result[0] != None:
         # draw search path
         optimal = result[0]
@@ -78,7 +80,7 @@ def print_explored_gui(maze, result):
 
             draw_maze(screen, maze, block_size)
             pygame.display.update()
-            time.sleep(0.1)
+            time.sleep(0.03)
 
     # wait for user to quit
     running = True
@@ -100,7 +102,7 @@ def search(maze, start, goal):
     cost = {}
 
     # add start node to the frontier with a priority value of 0
-    heapq.heappush(frontier, (0, start))
+    heapq.heappush(frontier, (0, 0, start))
     cost[start] = (0, None)
 
     for i in range(len(maze)):
@@ -109,8 +111,7 @@ def search(maze, start, goal):
                 cost[(i, j)] = (math.inf, None)
 
     while frontier:
-        # get the node with the lowest priority value
-        _, current = heapq.heappop(frontier)
+        _, _, current = heapq.heappop(frontier)
 
         # check if the current node is the goal node
         if current == goal:
@@ -119,7 +120,7 @@ def search(maze, start, goal):
             while current in cost:
                 path.append(current)
                 current = cost[current][1]
-            return list(reversed(path)), list(visited)
+            return list(path), list(visited)
 
         # add the current node to the visited set
         visited[current] = None
@@ -127,14 +128,15 @@ def search(maze, start, goal):
         # explore the neighbors of the current node
         for neighbor in get_neighbors(maze, current):
             # calculate the tentative actual cost to reach the neighbor node
-            tentative_cost = cost[current][0] + 1  # assuming each move has a cost of 1
+            tentative_cost = cost[current][0] + 1 # assuming each move has a cost of 1
 
             if tentative_cost < cost[neighbor][0]:
                 # update the actual cost and parent node for the neighbor node
                 cost[neighbor] = (tentative_cost, current)
-                priority = tentative_cost + heuristic(neighbor, goal)
+                hscore = heuristic(neighbor, goal)
+                priority = tentative_cost + hscore
                 # add the neighbor node to the frontier with the priority value
-                heapq.heappush(frontier, (priority, neighbor))
+                heapq.heappush(frontier, (priority, hscore, neighbor))
 
     # if the frontier is empty and the goal node has not been found, return None
     return None, visited
@@ -143,7 +145,6 @@ def get_neighbors(maze, node):
     # get the row and column of the current node
     row, col = node
 
-    # define the possible moves from the current node
     moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
     # check each possible move and add valid neighbors to the list
@@ -157,13 +158,11 @@ def get_neighbors(maze, node):
     return neighbors
 
 def heuristic(node, goal):
-    # need to change the heuristic calc, this is just manhattan distance, good for small mazes but very bad for
-    # bigger mazes
-    # i think we stick to manhattan - andre
-    return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+    # heuristic just manhattan
+    return (abs(node[0] - goal[0]) + abs(node[1] - goal[1]))
 
 def read_maze():
-    with open('mazes/maze19x19.txt', 'r') as f:
+    with open('mazes/maze_2.txt', 'r') as f:
         maze_size = int(f.readline())
         squares = [[0 for j in range(maze_size)] for i in range(maze_size)]
         for i in range(maze_size):
